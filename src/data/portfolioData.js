953e25e87a4890372d9c47f013b61dda1e2aca34
Jon Deavers <jondeavers@gmail.com>
2021-03-30 11:48:52 -0400

    Updated portfolio data

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -3,10 +3,9 @@ const portfolioData = [
     id: 1,
     name: "React Password Generator",
     screenshot: "/img/pword.png",
-    description:
-      "This is a simple password generator app built in ReactJS.",
+    description: "This is a simple password generator app built in ReactJS.",
     deployedURL: "https://practical-goldwasser-907445.netlify.app/",
-    githubURL: "https://github.com/lucsedirae/react-pword-gen"
+    githubURL: "https://github.com/lucsedirae/react-pword-gen",
   },
   {
     id: 2,
@@ -15,7 +14,7 @@ const portfolioData = [
     description:
       "Pitch is a music exploration application that helps music lovers track new artists, their catalogs, tour dates, and more. It was built by Jon Deavers, Tanner Kirkpatrick, David Stinnet, and Mark Major.",
     deployedURL: "https://lucsedirae.github.io/deep-cuts/",
-    githubURL: "https://github.com/lucsedirae/deep-cuts"
+    githubURL: "https://github.com/lucsedirae/deep-cuts",
   },
   {
     id: 3,
@@ -25,6 +24,15 @@ const portfolioData = [
       "Open House CRM is an open source customer relationship management tool for real estate professionals built by Jon Deavers, Melanie Hall, David Stinnet & Tanner Kirkpatrick. It focuses on mobile utility, making the user's data easily accessible in the field or behind a desktop.",
     deployedURL: "https://open-house-crm.herokuapp.com/",
     githubURL: "https://github.com/lucsedirae/open-house-crm",
+  },
+  {
+    id: 4,
+    name: "Website - The Fourth Rail Band",
+    screenshot: "/img/fourthrail.png",
+    description:
+      "A responsive website for the band 'The Fourth Rail Band'. Built in React.js it features responsive smooth-scrolling single page design as well as dynamic news and show date handling.",
+    deployedURL: "https://fourthrailband.com",
+    githubURL: null,
   },
 ];
 
